e556449de9ee54352935782f928fae0041222963
pakhare0111 <pakhare0111@gmail.com>
2024-12-22 17:26:31 +0530

    update 1.1

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -5,43 +5,43 @@
 const modeToggler = document.getElementById('darkmode');
 const documentBody = document.getElementsByTagName('body')[0];
 
+// Set default dark mode
+documentBody.classList.add('dark-mode');
+modeToggler.checked = true; // default to dark mode
 
 function setThemeFromCookie() {
-	// Check if the cookie is set 
-	if (typeof Cookies.get('mode') !== "undefined" ) {
-		documentBody.classList.add('dark-mode');
-		modeToggler.checked = true; // toggle change
-		
-		console.log('Cookie: dark mode' );
-		
+	// Check if the cookie is set
+	if (typeof Cookies.get('mode') !== "undefined") {
+		// Cookie exists, set the theme accordingly
+		if (Cookies.get('mode') === 'dark-mode') {
+			documentBody.classList.add('dark-mode');
+			modeToggler.checked = true; // toggle change
+			console.log('Cookie: dark mode');
+		} else {
+			documentBody.classList.remove('dark-mode');
+			modeToggler.checked = false; // toggle change
+			console.log('Cookie: light mode');
+		}
 	} else {
-		documentBody.classList.remove('dark-mode');
-		modeToggler.checked = false; // toggle change
-		
-		console.log('Cookie: light mode' );
+		// If no cookie is set, it's already dark mode by default
+		console.log('No cookie set: default dark mode');
 	}
 }
 
-
 setThemeFromCookie();
 
-
 modeToggler.addEventListener('change', () => {
-	
 	console.log(modeToggler.checked);
 	
     if (modeToggler.checked) {
 		documentBody.classList.add('dark-mode');
-		//Set cookies for 7 days 
+		// Set cookies for 7 days
 		Cookies.set('mode', 'dark-mode', { expires: 7 });
-		
-		console.log ('change to dark mode');
-		
+		console.log('Changed to dark mode');
 	} else {
 		documentBody.classList.remove('dark-mode');
-		//Remove cookies
+		// Remove cookies
 		Cookies.remove('mode');
-		
-		console.log ('change to light mode');
+		console.log('Changed to light mode');
 	}
 });
